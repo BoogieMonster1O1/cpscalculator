a95b85f6e33d62507792216530e52af493104176
BoogieMonster1O1 <shrish.deshpande@icloud.com>
2020-04-19 20:46:17 -0700

    Added Elements Interface

---
+++ src/boogie/cpscalc/Elements.java
@@ -0,0 +1,11 @@
+package boogie.cpscalc;
+
+import javax.swing.*;
+import java.awt.*;
+
+public interface Elements {
+
+    JLabel heading = new JLabel("Clicks Per Second Calculator",null,SwingConstants.CENTER);
+    JButton clicker = new JButton("Click me");
+
+}
--- src/boogie/cpscalc/Main.java
+++ src/boogie/cpscalc/Main.java
@@ -1,9 +1,31 @@
 package boogie.cpscalc;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
 
-public class Main {
+public class Main implements Elements{
+
+    public Main(){
+        JFrame frame = new JFrame();
+
+        clicker.setBounds(135,100,160,90);
+        heading.setBounds(135,0, 180,50);
+
+        frame.add(clicker);
+        frame.add(heading);
+        frame.setTitle("CPS Calculator");
+        frame.setResizable(false);
+        frame.setSize(450,450);
+        frame.setLayout(null);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public void actionPerformed(ActionEvent e){
+
+    }
+
     public static void main(String[] args) {
-	    JFrame f = new JFrame();
+	    new Main();
     }
 }
